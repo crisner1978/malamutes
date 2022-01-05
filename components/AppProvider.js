875f988804cf29dd6dabb66a4eea969808177c4c
Chris Risner <crisner.prsvr@gmail.com>
2022-01-05 09:03:33 -0600

    commit on main

--- components/AppProvider.js
+++ components/AppProvider.js
@@ -12,8 +12,8 @@ const AppProvider = ({ children }) => {
       <Head>
         <title>Snow Legend Malamutes</title>
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta name="description" content="Alaskan Malamute Puppies: Snow Legend Malamutes of Murfreesboro, TN 37130, Ten (10) Puppies available for sale/adoption." />
-        <meta property="og:title" title="Puppies Available: Alaskan Malamutes - Snow Legend Malamutes of Murfreesboro, TN 37130 are pleased to announce our first litter of TEN puppies born on 12/11/2021." />
+        <meta name="description" content="Puppies Available: Alaskan Malamute Puppies - Snow Legend Malamutes of Murfreesboro, TN 37130, has Ten (10) Puppies available for sale/adoption." />
+        <meta property="og:title" title="Puppies Available: Alaskan Malamute Puppies - Snow Legend Malamutes of Murfreesboro, TN 37130, is pleased to announce our first litter of TEN puppies born on 12/11/2021." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/snowLegend.png" />
       </Head>
